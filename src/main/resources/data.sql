d623b566ab915fba9e6258a9a39bb9acec1e7ce1
Luis Alejandro Santana <lsantana@hacienda.gov.do>
2019-08-22 17:23:57 -0400

    JPA Support, script load.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into user values(1, sysdate(), 'Jack');
+insert into user values(2, sysdate(), 'Martin');
+insert into user values(3, sysdate(), 'Adam');
+insert into user values(4, sysdate(), 'Rob');
+insert into user values(5, sysdate(), 'Jean');
