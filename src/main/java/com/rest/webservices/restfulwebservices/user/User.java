e4fa70b063f1fc12f78391cd73ad37822ace583f
Renovatio <luis.a.santana.sap@gmail.com>
2019-08-23 00:15:11 -0400

    Implementing Post entity, repository and resource.

--- src/main/java/com/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/rest/webservices/restfulwebservices/user/User.java
@@ -1,10 +1,13 @@
 package com.rest.webservices.restfulwebservices.user;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
@@ -26,6 +29,9 @@ public class User {
 	@Past
 	@ApiModelProperty(notes = "Birthdate should be in the past")
 	private Date birthDate;
+	
+	@OneToMany(mappedBy = "user")
+	private List<Post> posts = new ArrayList<>();
 
 	protected User() {
 	}
@@ -59,6 +65,14 @@ public class User {
 
 	public void setBirthDate(Date birthDate) {
 		this.birthDate = birthDate;
+	}
+
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
 	}
 
 	@Override
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,3 +3,5 @@ insert into user values(2, sysdate(), 'Martin');
 insert into user values(3, sysdate(), 'Adam');
 insert into user values(4, sysdate(), 'Rob');
 insert into user values(5, sysdate(), 'Jean');
+insert into post values(1100, 'My First Post', 1);
+insert into post values(1101, 'My Second Post', 1);
